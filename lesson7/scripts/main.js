8dbbbedf1b4513cf39d2a644fe47c0d97adb163d
norre-daroy <norrej.daroy@gmail.com>
2022-10-28 00:08:01 -0400

    Update lesson7

---
+++ lesson7/scripts/main.js
@@ -0,0 +1,29 @@
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+const imgOptions = {
+  threshold: 0.5,
+  rootMargin: '0px 0px 50px 0px',
+};
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver(
+    (items, observer) =>
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      }),
+    imgOptions
+  );
+  imagesToLoad.forEach((img) => observer.observe(img));
+} else {
+  imagesToLoad.forEach((img) => loadImages(img));
+}
