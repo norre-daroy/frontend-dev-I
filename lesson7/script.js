dda6977e439af083d6ed4995948dcdaf5dd0c606
norre-daroy <norrej.daroy@gmail.com>
2022-10-27 23:48:13 -0400

    Lazy loading images

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,15 +1,14 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const imgOptions = {
-  threshold: 0.5,
-  rootMargin: '0px 0px 50px 0px',
-};
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
+};
+
+const imgOptions = {
+  threshold: 0.5,
+  rootMargin: '0px 0px 50px 0px',
 };
 
 if ('IntersectionObserver' in window) {
