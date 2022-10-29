d99c5edbf2d7561c858e260185f7a130a5a1d1da
norre-daroy <norrej.daroy@gmail.com>
2022-10-28 23:23:25 -0400

    Add discover

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -1,30 +1,38 @@
+// Lazy loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+const imgOptions = {
+  threshold: 0.5,
+  rootMargin: '0px 0px 50px 0px',
+};
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver(
+    (items, observer) =>
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      }),
+    imgOptions
+  );
+  imagesToLoad.forEach((img) => observer.observe(img));
+} else {
+  imagesToLoad.forEach((img) => loadImages(img));
+}
+
 const currentDate = new Intl.DateTimeFormat('en-UK', {
   dateStyle: 'full',
 }).format(new Date());
 
-// // Locally store date of last visit
-window.localStorage.setItem('lastVisit', currentDate);
-
-// // Retrieve last visit date from local storage
-const lastVisit = localStorage.getItem('lastVisit');
-
-const date1 = new Date();
-const date2 = new Date(lastVisit);
-
-// // Calculate days between last visit
-const days = (date1, date2) => {
-  let difference = date1.getTime() - date2.getTime();
-  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-  return TotalDays;
-};
-
-const daysLastVisitText = () => {
-  const dayDiff = days(date1, date2);
-  return `${dayDiff} ${dayDiff > 1 ? 'days' : 'day'}`;
-};
-
-const displayLastVisit = daysLastVisitText();
-document.querySelector('#lastVisit').innerHTML = displayLastVisit;
 document.querySelector('#currentDate').innerHTML = currentDate;
 
 // Hamburger button toggle menu
